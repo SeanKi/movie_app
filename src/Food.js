c6b651016f65bc8bbb5f5df832f30fcbaec7a7b2
SeanKi <ykish72@gmail.com>
2021-07-11 19:14:15 +0900

    Use PropTypes

--- src/Food.js
+++ src/Food.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-function Food({name, picture}) {
+
+function Food({name, picture, rating}) {
     return (
         <div>
             <h2>I like {name}</h2>
+            <h4>{rating}/5.0</h4>
             <img src={picture} alt={name}/>
+
         </div>
     );
 }
